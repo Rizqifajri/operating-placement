9fa44e4b059b9abe2dde9ed00c55ff64ce02f942
Rizqi Fajri <rizqifajri51@gmail.com>
2025-07-07 12:06:38 +0700

    feat: add grid background at login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,8 +4,9 @@ import Image from "next/image";
 
 export default function Page () {
   return (
-    <section className="flex jusctify-center items-center h-screen">
-      <Card className="flex flex-row items-center justify-center w-[80%] mx-auto h-fit shadow-xl">
+    <section className="relative flex jusctify-center items-center h-screen">
+       <div className="absolute inset-0 z-0 bg-[linear-gradient(rgba(0,0,0,0.05)_1px,transparent_1px),linear-gradient(90deg,rgba(0,0,0,0.05)_1px,transparent_1px)] dark:bg-[linear-gradient(rgba(255,255,255,0.05)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.05)_1px,transparent_1px)] bg-[size:50px_50px] pointer-events-none" />
+      <Card className="flex flex-row items-center justify-center w-[60%] container mx-auto h-fit shadow-xl backdrop-blur-sm">
       <Image 
         className="hidden md:flex"
         src="/login-img.jpg"
